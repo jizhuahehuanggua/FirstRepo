97830e5d506d9fe95c46571d4597dd6c266d9220
yihan.wang3 <yihan.wang3@zone37-ed.cs.ucalgary.ca>
2020-02-04 09:23:51 -0700

    JUST TO GIVE A TRY

---
+++ Assignment1/src/ss1.java
@@ -0,0 +1,19 @@
+
+public class ss1 {
+	public static void main(String[] args) {
+		double sum = 0.0;
+		
+		for(int i = 0; i < 5 ; i++ ) 
+		{	
+			sum = sum + 3;
+			System.out.println(i);
+			
+		}
+		System.out.println(sum);
+	
+	}
+	
+	
+
+}
+
